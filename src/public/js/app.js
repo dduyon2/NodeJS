bd052e666f57fec0e93d3bba1103b48bf92ee0d2
user.email <ksy9500@naver.com>
2022-03-25 03:33:15 +0900

    making soketio server and rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,18 +1,90 @@
-//connect frontend to backend
-const socket = new WebSocket(`ws://${window.location.host}`);
+const socket = io();
 
-socket.addEventListener("open", ()=> {
-    console.log("Connected to Server ✔️");
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
+
+const nickname = document.getElementById("name");
+const nameForm = nickname.querySelector("form");
+
+const room = document.getElementById("room");
+
+welcome.hidden = true;
+room.hidden = true;
+
+let roomName;
+
+function addMessage(msg) {
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText=msg;
+    ul.append(li);
+}
+
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("#msg input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+    input.value="";
+}
+
+
+function inputRoomName() {
+    welcome.hidden=false;
+    nickname.hidden=true;
+    form.addEventListener("submit", handleRoomSubmit );
+}
+
+
+function handleRoomSubmit(event) {
+    event.preventDefault();
+    const input = form.querySelector("input");
+    socket.emit("enter_room", input.value, showRoom);    
+    roomName = input.value
+    input.value = "";
+}
+
+function showRoom() {
+    room.hidden=false;
+    form.hidden = true;
+    const h3 = room.querySelector("h3");
+    h3.innerText = `ROOM ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+}
+
+function handleNicknameSubmit(event) {
+    event.preventDefault();
+    const input = nameForm.querySelector("input");
+    const h4 = room.querySelector("h4");
+    h4.innerText = `USER ${input.value}`;
+    socket.emit("nickname", input.value, inputRoomName );
+}
+
+// form.addEventListener("submit", handleRoomSubmit);
+nameForm.addEventListener("submit", handleNicknameSubmit);
+
+socket.on("welcome", (user) => {
+    addMessage(`${user} arrived!`);
+});
+socket.on("bye", (left) => {
+    addMessage(`${left} left :(`);
 });
 
-socket.addEventListener("message", (message)=> {
-    console.log("Just got this:", message.data, "from the server ");
-});
-//server가 종료됐을 때
-socket.addEventListener("close", () =>{
-    console.log("Connected from Server ❌")
-});
+socket.on("new_message", addMessage);
 
-setTimeout(() => {
-    socket.send("hello from the browser!");
-}, 10000);
+socket.on("room_chane", (rooms)=> {
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML="";
+    if(rooms.length === 0) {
+        roomList.innerHTML = "";
+        return;
+    }
+    rooms.forEach( room => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    });
+});
--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 
 import http from "http";
-import WebSocket from "ws";
+// import WebSocket from "ws";
+import {Server} from "socket.io";
 import express from "express";
 import { handle } from "express/lib/application";
+import { Socket } from "dgram";
+import { publicDecrypt } from "crypto";
 
 const app = express();
 //pug로 view engine 설정
@@ -15,7 +18,7 @@ app.use("/public", express.static(__dirname + "/public"));
 //home.pug를 render해주는 handler
 app.get("/", (req, res)=> res.render("home"));
 //사용자가 어떤 url을 사용하던 홈으로 돌아올 수있도록 catchcall
-app.get("/*", (req, res) =>res.redirect("/"));
+// app.get("/*", (req, res) =>res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 // express는 http를 다루기 때문에 시작하는 법을 ws 로 하도록 두개의 프로토콜을 합칠거기 때문에 바꿈.
@@ -24,23 +27,77 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 //HTTP SERVER 
 //app : requestListener
 const server = http.createServer(app);
+const io = new Server(server);
+
+function publicRooms() {
+    const {
+        sockets: {
+            adapter: {sids, rooms},
+        },
+    } = io;
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined) {
+            publicRooms.push(key);
+        }
+    });
+    return publicRooms;
+}
+
+io.on("connection", (socket) => {
+    socket["nickname"] = "Anon";
+    socket.onAny((event) => {
+        console.log(`Socket Event: ${event}`);
+    })
+    socket.on("enter_room", (room, done)=>{
+        socket.join(room);
+        done();
+        socket.to(room).emit("welcome", socket.nickname);
+        io.sockets.emit("room_change", publicRooms());
+    } ) 
+    socket.on("disconnecting", () => {
+        socket.rooms.forEach((room)=> socket.to(room).emit("bye", socket.nickname));
+    })
+    socket.on("disconnect",() => {
+        io.sockets.emit("room_change", publicRooms());
+    } )
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
+    })
+    socket.on("nickname", (name, done) => {
+        socket["nickname"] = name;
+        console.log(socket.nickname);
+        done();
+    })
+})
 
 //WEBSOCKET SERVER
-const wss = new WebSocket.Server({ server });
+// const wss = new WebSocket.Server({ server });
 //line 25-28: http서버, websocket서버 둘 다 돌릴 수 있음 
 // 같은 서버에서 http, websocket둘다 작동시키는 것 두개가 같은 포트에 있길 원하기 때문에.
 // http 서버를 사용하지 않는다면 만들 line25는 없어도돼.
 
 
+/* const sockets = []
+
 wss.on("connection", (socket)=> {
+    sockets.push(socket)
+    socket["nickName"] = "Anon";
     console.log("Connected to Browser ✔️");
     //browser tab을 닫았을 때
     socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-    socket.on("message", (message)=> {
-        console.log(message.toString('utf-8'));
+    socket.on("message", (msg)=> {
+        const message = JSON.parse(msg);
+        switch(message.type) {
+            case "new_message": 
+            sockets.forEach((s)=>
+                s.send(`${socket.nickName}: ${message.payload.toString("utf-8")}`));
+            case "nickName" : 
+                socket["nickName"] = message.payload;
+        }
     });
-    socket.send("hello!!!");
-})
+}) */
 
 
 // 내 HTTP서버에 접근하기 위해 listen을 하는 거고 http위에 websocket 서버를 만들 수 있도록 한 것.
